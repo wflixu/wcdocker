a02fbb593f3102ee20fe427d97506f8293e8b183
lixu <wflixu@88.com>
2023-09-18 15:09:08 +0800

    github action

--- wcDockerLibrary.js
+++ wcDockerLibrary.js
@@ -1,44 +1,43 @@
 require.config({
-    baseUrl: "./bower_components",
-    shim: {
-        // Libraries
-        lodash: {
-            exports: '_'
-        }
+  baseUrl: "./bower_components",
+  shim: {
+    // Libraries
+    lodash: {
+      exports: "_",
     },
-    packages: [
-        {
-            name: 'dcl',
-            location: './dcl'   //points to bower_components/dcl
-        },
-        {
-            name: 'wcDocker',
-            location: '../Code'
-        },
-        {
-            name: 'lodash',
-            location: './lodash-compat'   //points to bower_components/dcl
-        }
-    ]
+  },
+  packages: [
+    {
+      name: "dcl",
+      location: "./dcl", //points to bower_components/dcl
+    },
+    {
+      name: "wcDocker",
+      location: "./Code",
+    },
+    {
+      name: "lodash",
+      location: "./lodash-compat", //points to bower_components/dcl
+    },
+  ],
 });
 
 require.config({
-    config: {}
+  config: {},
 });
 
 //require docker
 require([
-    "wcDocker/docker",
-    "wcDocker/splitter",
-    "wcDocker/tabframe",
-    "wcDocker/iframe",
-    "wcDocker/ThemeBuilder"
+  "wcDocker/docker",
+  "wcDocker/splitter",
+  "wcDocker/tabframe",
+  "wcDocker/iframe",
+  "wcDocker/ThemeBuilder",
 ], function (wcDocker, wcSplitter, wcTabFrame, wcIFrame, wcThemeBuilder) {
-
-    //export
-    window['wcDocker'] = wcDocker;
-    window['wcSplitter'] = wcSplitter;
-    window['wcTabFrame'] = wcTabFrame;
-    window['wcIFrame'] = wcIFrame;
-    window['wcThemeBuilder'] = wcThemeBuilder;
-}, undefined, true);    // Force synchronous loading so we don't have to wait.
+  //export
+  window["wcDocker"] = wcDocker;
+  window["wcSplitter"] = wcSplitter;
+  window["wcTabFrame"] = wcTabFrame;
+  window["wcIFrame"] = wcIFrame;
+  window["wcThemeBuilder"] = wcThemeBuilder;
+}, undefined, true); // Force synchronous loading so we don't have to wait.
